e937ee3204847eec9cd5b1030899166b719d91ff
Paul <paul@paulgoesdeep.com>
2019-12-28 12:21:59 +0300

    Remove unneeded object

--- dollarsigns.ts
+++ dollarsigns.ts
@@ -4,7 +4,4 @@ const $$$ = (selector: string): NodeListOf<HTMLElement> => (
   document.querySelectorAll(selector)
 );
 
-// I'm particularly proud of this line
-const DOM = {$: $, $$: $$, $$$: $$$};
-
-export { $, $$, $$$, DOM };
+export { $, $$, $$$ };
